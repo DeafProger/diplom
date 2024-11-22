ccf11b4ebb1fa679801b578c876aedfa17bc55fb
DeafProger <usvval@yandex.ru>
2024-11-22 08:09:37 +0700

    create admin

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Услуга')
+    description = models.TextField(max_length=250, verbose_name='Описание')
+    price = models.DecimalField(max_digits=8, decimal_places=2,
+                                verbose_name='Цена')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+        ordering = ['name']
+        permissions = [
+            ('can_add_service', 'Может добавлять услугу'),
+            ('can_change_service', 'Может изменять услугу'),
+            ('can_view_service', 'Может просматривать услугу'),
+        ]
