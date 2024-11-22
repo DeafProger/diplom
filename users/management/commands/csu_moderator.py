a553f2657254e74d230b4c8c5c1971b1d6068a07
DeafProger <usvval@yandex.ru>
2024-11-22 10:31:41 +0700

    fix karkas

--- users/management/commands/csu_moderator.py
+++ users/management/commands/csu_moderator.py
@@ -1,17 +1,16 @@
 from django.core.management import BaseCommand
-
 from users.models import User
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         user = User.objects.create(
-            email='health_moderator@localhost',
+            email='moderator@medcare.ru',
             first_name='admin',
-            password='12345',
+            password='medcare.ru',
             is_superuser=False,
             is_staff=True,
             is_active=True,
         )
-        user.set_password('Blev2011')
+        user.set_password('medcare.ru')
         user.save()
